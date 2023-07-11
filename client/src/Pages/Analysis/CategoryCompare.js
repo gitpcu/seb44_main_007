e10ab7ea3552a9068d7468200567ddf74eed00ed
LimJaeSub <liso_o@naver.com>
2023-07-11 14:55:58 +0900

    :zap: 우측 그래프 데이터 포팅

--- client/src/Pages/Analysis/CategoryCompare.js
+++ client/src/Pages/Analysis/CategoryCompare.js
@@ -14,15 +14,35 @@ function CategoryCompare({ spendData, lastmonthData }) {
   const prevmonth = lastmonthData;
 
   // 카테고리 종류
-  const categoryarr = [
+  const categoryarr1 = [
     ...new Set(
       curmonth.map((it) => {
         return it.category;
       })
     ),
   ];
+  const categoryarr2 = [
+    ...new Set(
+      prevmonth.map((it) => {
+        return it.category;
+      })
+    ),
+  ];
 
+  const categoryarr = Array.from(new Set([...categoryarr1, ...categoryarr2]));
+
+  // 이번달의 카테고리별 총 사용금액
   const curmonthByCategory = curmonth.reduce((result, item) => {
+    if (result[item.category]) {
+      result[item.category] += item.amount;
+    } else {
+      result[item.category] = item.amount;
+    }
+    return result;
+  }, {});
+
+  // 지난달의 카테고리별 총 사용금액
+  const lastmonthBycategory = prevmonth.reduce((result, item) => {
     if (result[item.category]) {
       result[item.category] += item.amount;
     } else {
@@ -33,8 +53,19 @@ function CategoryCompare({ spendData, lastmonthData }) {
   return (
     <Wrap>
       {categoryarr.map((it) => {
-        const categoryamount = curmonthByCategory[it];
-        return <CategoryCompageDetail categoryamount={categoryamount} />;
+        const categoryamount = curmonthByCategory[it]
+          ? curmonthByCategory[it]
+          : 0;
+        const lastmonthcategoryamount = lastmonthBycategory[it]
+          ? lastmonthBycategory[it]
+          : 0;
+        return (
+          <CategoryCompageDetail
+            categoryamount={categoryamount}
+            lastmonthcategoryamount={lastmonthcategoryamount}
+            category={it}
+          />
+        );
       })}
     </Wrap>
   );
