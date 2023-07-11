f68bc6e712fc99a6afc52a0ea0d80ff5dbf75492
이수민 <124749717+tnals9707@users.noreply.github.com>
2023-07-11 17:20:39 +0900

    feat: Update Store.js

--- client/src/Redux/Store.js
+++ client/src/Redux/Store.js
@@ -1,10 +1,21 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import dataSlice from "./reducers";
+import { expendReducer, profitReducer, targetReducer,  } from './account_reducer';
+import dataReducer from './submit_data_reducer';
+import selectedDateReducer from './date_reducer'
+
+const rootReducer = combineReducers({
+  data: dataSlice,
+  totalExpend: expendReducer, //지출 총 합
+  totalProfit: profitReducer, //수익 총 합
+  targetExpend: targetReducer, //목표 지출 금액
+  accountData: dataReducer, // 데이터 리스트
+  selectedDate: selectedDateReducer, //선택한 날짜
+  
+});
 
 const store = configureStore({
-  reducer: {
-    data: dataSlice,
-  },
+  reducer: rootReducer,
 });
 
 export default store;
